49d95c1d067ce76630e0b18fa65a4a5ad521f414
qiaokun <258407@st.takushoku-u.ac.jp>
2026-05-14 16:49:55 +0900

    1

--- practice-eventdom/tashizan.js
+++ practice-eventdom/tashizan.js
@@ -1,20 +1,9 @@
-
 let x = document.querySelector('button#calc');
-
-
 x.addEventListener('click', add);
-
-
 function add(){
     let a = document.querySelector('input[name="left"]');
     let b = document.querySelector('input[name="right"]');
-    
-    
     let sum = Number(a.value) + Number(b.value);
-    
-    
     let p = document.querySelector('span#answer');
-    
-    
     p.textContent = sum;
 }

090849b557339b9d5f2c78532bcf828cb37a2c56
qiaokun <258407@st.takushoku-u.ac.jp>
2026-05-21 15:29:00 +0900

    1

--- practice-dom2/dom2-exercise.js
+++ practice-dom2/dom2-exercise.js
@@ -16,3 +16,32 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+function show() {
+    let p = document.createElement('p');
+    p.textContent = '八王子市館町';
+    let h2Addr = document.querySelector('h2#addr');
+    h2Addr.insertAdjacentElement('afterend', p);
+
+    let u = document.createElement('ul');
+    let h2Dept = document.querySelector('h2#dept');
+    h2Dept.insertAdjacentElement('afterend', u);
+
+    let l1 = document.createElement('li');
+    u.insertAdjacentElement('beforeend', l1);
+    l1.textContent = '机械システム工学科';
+
+    let l2 = document.createElement('li');
+    u.insertAdjacentElement('beforeend', l2);
+    l2.textContent = '電子システム工学科';
+
+    let l3 = document.createElement('li');
+    u.insertAdjacentElement('beforeend', l3);
+    l3.textContent = '情報工学科';
+
+    let l4 = document.createElement('li');
+    u.insertAdjacentElement('beforeend', l4);
+    l4.textContent = 'デザイン学科';
+}
+
+let b = document.querySelector('button#show');
+b.addEventListener('click', show);

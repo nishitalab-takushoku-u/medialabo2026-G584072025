45af03c788b63d99d3194e47d9355b4c196c901a
qiaokun <258407@st.takushoku-u.ac.jp>
2026-05-26 11:55:47 +0900

    1

--- practice-html/weather.js
+++ practice-html/weather.js
@@ -17,21 +17,28 @@ function print(data) {
 
 // 課題5-1 の関数 printDom() はここに記述すること
 function printDom(data) {
-let d = document. createElement('div');
-d. setAttribute('id', 'result') ;
-d.textContent = 
-  console.log("--- 検索結果 ---");
-  console.log("都市名: " + data.name);
-  console.log("緯度: " + data.coord.lat + ", 経度: " + data.coord.lon);
-  console.log("天气: " + data.weather[0].description);
-  console.log("最高気温: " + data.main.temp_max + "℃");
-  console.log("最低気温: " + data.main.temp_min + "℃");
-  console.log("湿度: " + data.main.humidity + "%");
-  console.log("風速: " + data.wind.speed + "m/s");
-  console.log("風向: " + data.wind.deg + "度");
-let b = document. querySelector('body');
-b. insertAdjacentElement( 'beforeend', d);
-}
+    let oldResult = document.getElementById('result');
+    if (oldResult) {
+        oldResult.remove();
+    }
+    let d = document.createElement('div');
+    d.setAttribute('id', 'result');
+
+    d.innerHTML = `
+        <p>--- 検索結果 ---</p >
+        <p>都市名: ${data.name}</p >
+        <p>緯度: ${data.coord.lat}, 経度: ${data.coord.lon}</p >
+        <p>天気: ${data.weather[0].description}</p >
+        <p>最高気温: ${data.main.temp_max}℃</p >
+        <p>最低気温: ${data.main.temp_min}℃</p >
+        <p>湿度: ${data.main.humidity}%</p >
+        <p>風速: ${data.wind.speed} m/s</p >
+        <p>風向: ${data.wind.deg}度</p >
+    `;
+
+    let b = document.querySelector('body');
+    b.insertAdjacentElement('beforeend', d);
+  }
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
 
